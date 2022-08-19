be2abcc8b81ec197ce31f9025a7ea6250951c9ea
mannawar <dearmannawar@gmail.com>
2022-08-19 08:12:56 +0300

    Preswitch to postgres section 19

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-test-errors',
@@ -8,7 +9,7 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./test-errors.component.css']
 })
 export class TestErrorsComponent implements OnInit {
-  baseUrl = 'https://localhost:5001/api/'
+  baseUrl = environment.apiUrl;
   validationErrors: string[] = [];
 
   constructor( private http: HttpClient, private toastr: ToastrService ) { }
